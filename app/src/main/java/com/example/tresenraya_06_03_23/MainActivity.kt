fe580d1dadf94ec150ba7153b9e6810aa2e222f9
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-03-06 13:37:56 +0100

    Añadido Layout y binding: Forzado en modo landscape

--- app/src/main/java/com/example/tresenraya_06_03_23/MainActivity.kt
+++ app/src/main/java/com/example/tresenraya_06_03_23/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.tresenraya_06_03_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tresenraya_06_03_23.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Base() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
+
 }
